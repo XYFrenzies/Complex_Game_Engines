863e7bfba4b037cf331e2a4404afe88d36b30dd0
Ben <bennybill16@gmail.com>
2021-04-22 15:21:36 +1000

    I have added a new unity project witha few scripts attached to it.

---
+++ Complex Game Systems RPG/Assets/Scripts/Modular RPG Files/Editor/CustomEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Entities))]
+[CanEditMultipleObjects]
+public class CustomEditorScript : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Entities entity = (Entities)target;
+        
+    }
+}
---
+++ Complex Game Systems RPG/Assets/Scripts/Modular RPG Files/Entities.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entities
+{
+    float m_health = 0;
+    float attDMG = 0;
+    float def = 0;
+
+
+
+    float[] m_primAttrib;
+    float[] m_secAttrib;
+
+}
